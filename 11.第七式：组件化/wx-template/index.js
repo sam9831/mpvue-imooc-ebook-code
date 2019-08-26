227e7749201265f869e4019e00c54d63f4ccebf7
sam <247765564@qq.com>
2019-08-26 23:01:26 +0800

    wx-template

---
+++ 11.第七式：组件化/wx-template/index.js
@@ -0,0 +1,11 @@
+import item from './item.js'
+
+Page({
+  data: {
+    message: '第七式：组件化',
+    itemData: {
+      message: 'Hello Mpvue'
+    }
+  },
+  ...item
+})
---
+++ 11.第七式：组件化/wx-template/item.js
@@ -0,0 +1,5 @@
+export default {
+  onClick: function (event) {
+    console.log(event)
+  }
+}
