06894137d00c1225b3a026a419f36ecea8296d62
247765564@qq.com <247765564@qq.com>
2019-09-01 21:32:26 +0800

    mpvue-component

---
+++ 11.第七式：组件化/mpvue-component/src/components/Item.vue
@@ -0,0 +1,16 @@
+<!-- Item.vue -->
+<template>
+  <div>
+    <div>This is Header</div>
+    <slot name="header"></slot>
+    <div>This is Body</div>
+    <slot></slot>
+    <div>This is Footer</div>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
---
+++ 11.第七式：组件化/mpvue-component/src/pages/index/main.vue
@@ -0,0 +1,20 @@
+<!-- index.vue -->
+<template>
+  <div>
+    <my-item>
+      <div>body...</div>
+      <div slot="header">header...</div>
+      <div slot="footer">footer...</div>
+    </my-item>
+  </div>
+</template>
+
+<script>
+import MyItem from '../../components/Item'
+
+export default {
+  components: {
+    MyItem
+  }
+}
+</script>
